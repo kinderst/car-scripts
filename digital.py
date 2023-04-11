5a663b9ca968e92855ccfadf1034669cedbbaa67
kinderst <kinders@uw.edu>
2023-04-10 20:44:50 -0600

    sdf

--- digital.py
+++ digital.py
@@ -4,26 +4,26 @@ import digitalio
 
 import inspect
 
-print("wtf")
-print(inspect.getmembers(board))
-print("ok")
-print(dir(board))
+# print("wtf")
+# print(inspect.getmembers(board))
+# print("ok")
+# print(dir(board))
 
 # Set up digital input pin
-# digital_in = digitalio.DigitalInOut(board.XU4_GPIO.B)
-# digital_in.direction = digitalio.Direction.INPUT
+digital_in = digitalio.DigitalInOut(board.D16)
+digital_in.direction = digitalio.Direction.INPUT
 
-# # Loop forever, reading and printing the digital input value
-# while True:
-#     # Read the digital input value
-#     digital_value = digital_in.value
+# Loop forever, reading and printing the digital input value
+while True:
+    # Read the digital input value
+    digital_value = digital_in.value
    
-#     # If the digital input value is high, the sensor is detecting an object
-#     # If the digital input value is low, the sensor is not detecting an object
-#     if digital_value:
-#         print("Object detected")
-#     else:
-#         print("No object detected")
+    # If the digital input value is high, the sensor is detecting an object
+    # If the digital input value is low, the sensor is not detecting an object
+    if digital_value:
+        print("Object detected")
+    else:
+        print("No object detected")
    
-#     # Delay for a short period of time before repeating the loop
-#     time.sleep(0.1)
+    # Delay for a short period of time before repeating the loop
+    time.sleep(0.1)
