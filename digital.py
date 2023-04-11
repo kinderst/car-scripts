2a7e5ddb33782720b61afab32bccaa3281a9da26
kinderst <kinders@uw.edu>
2023-04-10 20:13:08 -0600

    dfg

---
+++ digital.py
@@ -0,0 +1,22 @@
+import time
+import board
+import digitalio
+
+# Set up digital input pin
+digital_in = digitalio.DigitalInOut(board.GP16)
+digital_in.direction = digitalio.Direction.INPUT
+
+# Loop forever, reading and printing the digital input value
+while True:
+    # Read the digital input value
+    digital_value = digital_in.value
+   
+    # If the digital input value is high, the sensor is detecting an object
+    # If the digital input value is low, the sensor is not detecting an object
+    if digital_value:
+        print("Object detected")
+    else:
+        print("No object detected")
+   
+    # Delay for a short period of time before repeating the loop
+    time.sleep(0.1)
